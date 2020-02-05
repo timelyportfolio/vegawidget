ce69d8c55144ab4641fa1885b50688bdd1efb037
timelyportfolio <kent.russell@timelyportfolio.com>
2020-02-04 18:35:16 -0600

    finish example with not great results but good learning

--- inst/test-apps/event/app.R
+++ inst/test-apps/event/app.R
@@ -12,23 +12,51 @@ spec <-
 
 ui <- shiny::fluidPage(
   tags$script(HTML("
+    // hack to also dispatch a native DOM event on every jQuery shiny:inputchanged
     $(document).on('shiny:inputchanged', function(e) {
-      var event = new CustomEvent('shiny_inputchanged', { detail: e });
+      var event = new CustomEvent('shinynative:inputchanged', { detail: e });
       window.dispatchEvent(event);
     })
   ")),
   shiny::titlePanel("vegawidget signal example"),
+  tags$p(
+"
+I had hoped to demonstrate vega signals based on shiny JS events.  Unfortunately, shiny JS events
+are jQuery triggered events and not native DOM events.  Vega only sees native DOM events.  We
+can overcome with a hack, but this is likely not a viable robust strategy going forward.  For
+future reference, I thought a quick example would be helpful.
+"
+  ),
   shiny::fluidRow(
-    shiny::sliderInput(
-      "slider",
-      label = "Cylinders",
-      min = 4,
-      max = 8,
-      step = 2,
-      value = 4
+    column(
+      width = 3,
+      shiny::sliderInput(
+        "slider",
+        label = "Cylinders",
+        min = 4,
+        max = 8,
+        step = 2,
+        value = 4
+      )
+    ),
+    column(
+      width = 3,
+      tags$input(
+        type = "range",
+        id = "domslider",
+        min = "4",
+        max = "8",
+        value = "6",
+        step = 2
+      )
     )
   ),
-  shiny::fluidRow(vegawidgetOutput("chart")),
+  shiny::fluidRow(
+    tagList(
+      h3("vega chart"),
+      vegawidgetOutput("chart")
+    )
+  ),
   shiny::fluidRow(shiny::verbatimTextOutput("cl"))
 
 )
